0279697b72efdaa2047f8caf8d4c8f68391a3aef
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-30 17:47:49 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\s-']+/g, "");
+  }
+  static titleize(string) {
+    const words = string.split(' ');
+    const newWords = words.map(function(el) {
+      if (el === 'the' || el === 'a' || el === 'an' || el === 'but' || el === 'of' || el === 'and' || el === 'for' || el === 'at' || el === 'by' || el === 'from') {
+        if (el === words[0]) {
+          return Formatter.capitalize(el);
+        } else {
+          return el;
+        }
+      } else {
+        return Formatter.capitalize(el);
+      }
+    })
+    return newWords.join(' ');
+  }
 }
